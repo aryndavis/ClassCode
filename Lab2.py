dbe4d8faa4463106c1198ddd8e5983bfd08a6635
“Aryn <arynlynn1@gmail.com>
2021-06-23 17:17:48 -0500

    added variable practice, creating them and type()

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+name = 'LeBron'
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = '23'
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
